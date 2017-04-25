60219ce1bad392ca5c84bd530d1cea0dad5d35f7
luisbarajasp <luisbarajasp@gmail.com>
2017-04-25 01:16:40 -0500

    Fix: graphs for working with rentas

--- dashboard/pages/index.php
+++ dashboard/pages/index.php
@@ -292,7 +292,7 @@
 
                 $array = array();
 
-                $string = "select count(mueble_cliente.noRenta) as numero, sum(mueble_cliente.cantidadRentada * mueble_cliente.precio) as ingreso from mueble_cliente where  mueble_cliente.inicio < '$fechaMax' AND ((mueble_cliente.fin > '$fechaMin' AND mueble_cliente.fin < '$fechaMax') OR mueble_cliente.fin IS NULL);";
+                $string = "select count(renta.noRenta) as numero, sum(renta.total) as ingreso from renta where renta.inicio < '$fechaMax' AND ((renta.fin > '$fechaMin' AND renta.fin < '$fechaMax') OR renta.fin IS NULL);";
 
                 $query = mysqli_query($enlace, $string);
 
