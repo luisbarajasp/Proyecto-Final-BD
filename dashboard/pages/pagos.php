16cd894200b9a0df22b8c03ea9f4758a9e691a52
luisbarajasp <luisbarajasp@gmail.com>
2017-04-26 21:36:50 -0500

    Add: get_pagos_date by fecha de pago via ajax

--- dashboard/pages/pagos.php
+++ dashboard/pages/pagos.php
@@ -82,7 +82,7 @@ include("../functions/functions.php");
                                         <div class="col-lg-12">
                                             <div class="form-group">
                                                 <label>Cliente</label>
-                                                <select class="form-control" name="id" required onchange="showUser(this.value)">
+                                                <select class="form-control" name="id" required onchange="showPayUser(this.value)">
                                                     <option value="">--</option>
                                                     <?php
 
@@ -98,6 +98,75 @@ include("../functions/functions.php");
 
                                                      ?>
                                                 </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                            <!-- /.row (nested) -->
+                            <div class="row">
+                                <div class="table-responsive" id="show-results"></div>
+                            </div>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <div class="panel panel-default panel-form" id="fecha">
+                        <div class="panel-body">
+                            <div class="row">
+                                <form role="form" class="porCliente" onsubmit="showPayDate()">
+                                    <div class="col-lg-12">
+                                        <div class="col-lg-12">
+                                            <div class="form-group">
+                                                <div class="col-xs-12">
+                                                    <label>Fecha</label>
+
+                                                </div>
+                                                <div class="col-xs-3">
+                                                    <select class="form-control" name="day" id="day" required>
+                                                        <option value="">Dia</option>
+                                                        <?php
+
+                                                            for ($i=1; $i < 32; $i++) {
+                                                                if($i < 10){
+                                                                    $e = "0" . $i;
+                                                                }else{
+                                                                    $e = $i;
+                                                                }
+                                                                echo "<option value='$e'>$e</option>";
+                                                            }
+
+                                                         ?>
+                                                    </select>
+                                                </div>
+                                                <div class="col-xs-3">
+                                                    <select class="form-control" name="month" id="month" required>
+                                                        <option value="">Mes</option>
+                                                        <?php
+
+                                                        for ($i=1; $i < 13; $i++) {
+                                                            if($i < 10){
+                                                                $e = "0" . $i;
+                                                            }else{
+                                                                $e = $i;
+                                                            }
+                                                            echo "<option value='$e'>$e</option>";
+                                                        }
+
+                                                         ?>
+                                                    </select>
+                                                </div>
+                                                <div class="col-xs-3">
+                                                    <select class="form-control" name="year" id="year" required onchange="showPayDate()">
+                                                        <option value="">Año</option>
+                                                        <?php
+
+                                                        for ($i=2017; $i < 2030; $i++) {
+                                                            echo "<option value='$i'>$i</option>";
+                                                        }
+
+                                                         ?>
+                                                    </select>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
@@ -140,7 +209,7 @@ include("../functions/functions.php");
 
 </html>
  <script type="text/javascript">
-         function showUser(str) {
+         function showPayUser(str) {
             if (str == "") {
                 document.getElementById("show-results").innerHTML = "";
                 return;
@@ -154,13 +223,44 @@ include("../functions/functions.php");
                 }
                 xmlhttp.onreadystatechange = function() {
                     if (this.readyState == 4 && this.status == 200) {
-                        document.getElementById("show-results").innerHTML = this.responseText;
+                        // div = document.getElementById("cliente");
+                        // div.getElementById("show-results").innerHTML = this.responseText;
+                        jQuery('#cliente #show-results').html(this.responseText);
                     }
                 };
                 xmlhttp.open("GET","get_pagos.php?q="+str,true);
                 xmlhttp.send();
             }
         }
+        function showPayDate() {
+            str = document.getElementById("year").value;
+            str += "-";
+            str += document.getElementById("month").value;
+            str += "-"
+            str += document.getElementById("day").value;
+           if (str.length < 9) {
+               document.getElementById("show-results").innerHTML = "";
+               return;
+           } else {
+               if (window.XMLHttpRequest) {
+                   // code for IE7+, Firefox, Chrome, Opera, Safari
+                   xmlhttp = new XMLHttpRequest();
+               } else {
+                   // code for IE6, IE5
+                   xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+               }
+               xmlhttp.onreadystatechange = function() {
+                   if (this.readyState == 4 && this.status == 200) {
+                    //    div = document.getElementById("fecha");
+                    //    div.getElementById("show-results").innerHTML = this.responseText;
+                       jQuery('#fecha #show-results').html(this.responseText);
+                   }
+               };
+               xmlhttp.open("GET","get_pagos_date.php?q="+str,true);
+               xmlhttp.send();
+           }
+           return false;
+       }
         $(function(){
             $('.form-link').on('click',function(){
                 id = $(this).find('a').attr("href");
